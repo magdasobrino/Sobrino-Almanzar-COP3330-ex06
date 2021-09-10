49ce2ccb4e2fc2e7d74404f14e8c6cb0b63bef5b
magdasobrino <magdalena.sobrino@knights.ucf.edu>
2021-09-09 20:16:56 -0400

    Initial commit

---
+++ src/main/java/Exercise06.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Magdalena Sobrino-Almanzar
+ */
+package exercise;
+
+import java.util.Scanner;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Exercise06 {
+    public static void main(String[] args) {
+
+
+
+        int currentyear;
+        int futureyear;
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your current age? ");
+        String age = input.next();
+
+        System.out.print("At what age would you like to retire? ");
+        String retireage = input.next();
+
+        int agenow = Integer.parseInt(age);
+        int agebefore = Integer.parseInt(retireage);
+
+        int subyears = agebefore - agenow;
+
+        LocalDate currentDate = LocalDate.now();
+        DayOfWeek dow = currentDate.getDayOfWeek();
+
+
+        int yearnow = currentDate.getYear();
+        int yearthen = yearnow + subyears;
+
+        System.out.print(" You have " + subyears + " left until you can retire.");
+        System.out.println("It's " + yearnow +", so you can retire in " +yearthen + ".");
+
+
+    } // end of main//
+
+} // end of program//
